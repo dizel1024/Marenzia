14d637952e77c11cf6aff9a2d06981cf2a54311d
“dizel1024 <kizoobco@gmail.com>
2026-03-12 08:15:48 +0200

    fix: resolve Vercel deployment ERR_INVALID_URL on GraphQL client initialization

--- lib/hygraph.ts
+++ lib/hygraph.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
-const contentApi = process.env.HYGRAPH_CONTENT_API || '';
-const managementApi = process.env.HYGRAPH_MANAGEMENT_API || '';
+const contentApi = process.env.HYGRAPH_CONTENT_API || 'https://api-eu-west-2.hygraph.com/v2/cmmjbw97104i108lcgwhdessh/master';
+const managementApi = process.env.HYGRAPH_MANAGEMENT_API || 'https://api-eu-west-2.hygraph.com/v2/cmmjbw97104i108lcgwhdessh/master';
 const token = process.env.HYGRAPH_TOKEN || '';
 
 export const hygraph = new GraphQLClient(contentApi, {
